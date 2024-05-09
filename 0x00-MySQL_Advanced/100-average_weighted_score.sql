7f3fb8835c853e4c7cfc2f8a4159bc313f291c5b
Brokeice123 <keithmarkomen@gmail.com>
2024-05-09 19:55:36 +0300

    Output error

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,9 +6,9 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
   UPDATE users 
   SET average_weighted_score = (
-    SELECT AVG(score * projects.weight)
+    SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
     FROM corrections
-    JOIN projects ON projects.id = corrections.project_id
+    INNER JOIN projects ON projects.id = corrections.project_id
     WHERE corrections.user_id = user_id
   )
   WHERE id = user_id;
