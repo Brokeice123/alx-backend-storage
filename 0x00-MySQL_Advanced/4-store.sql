d0153abba0dfa1531f29f96b01a4acd114141e08
Brokeice123 <keithmarkomen@gmail.com>
2024-05-09 13:52:25 +0300

    Editing

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,14 +2,12 @@
 -- the quantity of an item after adding a new order
 
 DELIMITER //
-
-CREATE TRIGGER decrease_quantity_after_order_creation
-AFTER INSERT ON orders
+CREATE TRIGGER decrease_quantity_after_order_creation AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.quantity
-    WHERE id = NEW.item_id;
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
 END //
 
 DELIMITER ;
